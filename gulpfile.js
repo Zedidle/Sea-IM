eec7820076e09a720da4e16e022d7b717756469d
zfcheng <476409251@qq.com>
2017-12-03 17:36:00 +0800

    0.9.9.3

--- gulpfile.js
+++ gulpfile.js
@@ -13,15 +13,12 @@ var
 
 
 gulp.task('default',['clean'],function(){
-
 	gulp.start( 'lib', 'mongoModel','public_less','public_cropper','public_js','public_img','public_voice','routes','views','app');
-
 })
 gulp.task('clean', function() { 
   return gulp.src(['dist'], {read: false})
     .pipe(clean());
 });
-
 gulp.task('lib', function() {
 	gulp.src('./dev/lib/*.js')
 	.pipe(gulp.dest('./dist/lib'));
@@ -95,91 +92,3 @@ gulp.task('watch', function() {
   });
  
 });
-
-
-
-
-
-
-
-
-// //载入外挂
-// var gulp = require('gulp'), 
-//     less = require('gulp-less'),
-//     autoprefixer = require('gulp-autoprefixer'),
-//     minifycss = require('gulp-minify-css'),
-//     jshint = require('gulp-jshint'),
-//     uglify = require('gulp-uglify'),
-//     imagemin = require('gulp-imagemin'),
-//     rename = require('gulp-rename'),
-//     clean = require('gulp-clean'),
-//     concat = require('gulp-concat'),
-//     notify = require('gulp-notify'),
-//     cache = require('gulp-cache'),
-//     livereload = require('gulp-livereload');
- 
-// // 样式
-// gulp.task('styles', function() { 
-//   return gulp.src('src/styles/main.scss')
-//     .pipe(sass({ style: 'expanded', }))
-//     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-//     .pipe(gulp.dest('dist/styles'))
-//     .pipe(rename({ suffix: '.min' }))
-//     .pipe(minifycss())
-//     .pipe(gulp.dest('dist/styles'))
-//     .pipe(notify({ message: 'Styles task complete' }));
-// });
- 
-// // 脚本
-// gulp.task('scripts', function() { 
-//   return gulp.src('src/scripts/**/*.js')
-//     .pipe(jshint('.jshintrc'))
-//     .pipe(jshint.reporter('default'))
-//     .pipe(concat('main.js'))
-//     .pipe(gulp.dest('dist/scripts'))
-//     .pipe(rename({ suffix: '.min' }))
-//     .pipe(uglify())
-//     .pipe(gulp.dest('dist/scripts'))
-//     .pipe(notify({ message: 'Scripts task complete' }));
-// });
- 
-// // 图片
-// gulp.task('images', function() { 
-//   return gulp.src('src/images/**/*')
-//     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-//     .pipe(gulp.dest('dist/images'))
-//     .pipe(notify({ message: 'Images task complete' }));
-// });
- 
-// // 清理
-// gulp.task('clean', function() { 
-//   return gulp.src(['dist/styles', 'dist/scripts', 'dist/images'], {read: false})
-//     .pipe(clean());
-// });
- 
-// // 预设任务
-// gulp.task('default', ['clean'], function() { 
-//     gulp.start('styles', 'scripts', 'images');
-// });
- 
-// // 看守
-// gulp.task('watch', function() {
- 
-//   // 看守所有.scss档
-//   gulp.watch('src/styles/**/*.scss', ['styles']);
- 
-//   // 看守所有.js档
-//   gulp.watch('src/scripts/**/*.js', ['scripts']);
- 
-//   // 看守所有图片档
-//   gulp.watch('src/images/**/*', ['images']);
- 
-//   // 建立即时重整伺服器
-//   var server = livereload();
- 
-//   // 看守所有位在 dist/  目录下的档案，一旦有更动，便进行重整
-//   gulp.watch(['dist/**']).on('change', function(file) {
-//     server.changed(file.path);
-//   });
- 
-// });
